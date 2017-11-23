7d77719626adf640244a8caa1334990ab7c8f019
wmcheung <wesleylgr33@gmail.com>
2017-11-23 18:24:57 +0100

    added login register screen with scripts and optimized map structure

---
+++ Assets/Scripts/LoginComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginComponent : MonoBehaviour {
+
+	public InputField username;
+	public InputField password;
+
+	private string Username;
+	private string Password;
+	private string form;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Username = username.text;
+		Password = password.text;
+
+//		Debug.Log (Username);
+	}
+}
